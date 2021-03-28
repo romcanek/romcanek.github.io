becc5ba482b33671e5b4090a191bd062c29f9704
romcanek <71186827+romcanek@users.noreply.github.com>
2021-03-28 13:01:17 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,95 +1,96 @@
 var predesla_hodnota = 0
-var hodnota_ted;
+var hodnota_ted = 0
 var decimal_ready = true;
 var operace;
 function addNum7(){
-    if(document.getElementById("input").value == 0 && document.getElementById("input").value != "0.")
+    if(String(document.getElementById("input").value) == '0')
     {
-        document.getElementById("input").value = String(7);
+        document.getElementById("input").value = 7;
     }
     else{
-        document.getElementById("input").value += String(7);
+        document.getElementById("input").value += 7;
     }    
 }
 function addNum8(){
-    if(document.getElementById("input").value == 0 && document.getElementById("input").value != "0.")
+    if(String(document.getElementById("input").value) == '0')
     {
-        document.getElementById("input").value = String(8);
+        document.getElementById("input").value = 8;
     }
     else{
-        document.getElementById("input").value += String(8);
-    } 
+        document.getElementById("input").value += 8;
+    }     
 }
 function addNum9(){
-    if(document.getElementById("input").value == 0 && document.getElementById("input").value != "0.")
+    if(String(document.getElementById("input").value) == '0')
     {
-        document.getElementById("input").value = String(9);
+        document.getElementById("input").value = 9;
+
     }
     else{
-        document.getElementById("input").value += String(9);
-    } 
+        document.getElementById("input").value += 9;
+    }    
 }
 function addNum4(){
-    if(document.getElementById("input").value == 0 && document.getElementById("input").value != "0.")
+    if(String(document.getElementById("input").value) == '0')
     {
-        document.getElementById("input").value = String(4);
+        document.getElementById("input").value = 4;
     }
     else{
-        document.getElementById("input").value += String(4);
-    } 
+        document.getElementById("input").value += 4;
+    }     
 }
 function addNum5(){
-    if(document.getElementById("input").value == 0 && document.getElementById("input").value != "0.")
+    if(String(document.getElementById("input").value) == '0')
     {
-        document.getElementById("input").value = String(5);
+        document.getElementById("input").value = 5;
     }
     else{
-        document.getElementById("input").value += String(5);
-    } 
+        document.getElementById("input").value += 5;
+    }   
 }
 function addNum6(){
-    if(document.getElementById("input").value == 0 && document.getElementById("input").value != "0.")
+    if(String(document.getElementById("input").value) == '0')
     {
-        document.getElementById("input").value = String(6);
+        document.getElementById("input").value = 6;
     }
     else{
-        document.getElementById("input").value += String(6);
-    } 
+        document.getElementById("input").value += 6;
+    }   
 }
 function addNum1(){
-    if(document.getElementById("input").value == 0 && document.getElementById("input").value != "0.")
+    if(String(document.getElementById("input").value) == '0')
     {
-        document.getElementById("input").value = String(1);
+        document.getElementById("input").value = 1;
     }
     else{
-        document.getElementById("input").value += String(1);
-    } 
+        document.getElementById("input").value += 1;
+    }   
 }
 function addNum2(){
-    if(document.getElementById("input").value == 0 && document.getElementById("input").value != "0.")
+    if(String(document.getElementById("input").value) == '0')
     {
-        document.getElementById("input").value = String(2);
+        document.getElementById("input").value = 2;
     }
     else{
-        document.getElementById("input").value += String(2);
-    } 
+        document.getElementById("input").value += 2;
+    }    
 }
 function addNum3(){
-    if(document.getElementById("input").value == 0 && document.getElementById("input").value != "0.")
+    if(String(document.getElementById("input").value) == '0')
     {
-        document.getElementById("input").value = String(3);
+        document.getElementById("input").value = 3;
     }
     else{
-        document.getElementById("input").value += String(3);
-    } 
+        document.getElementById("input").value += 3;
+    }   
 }
 function addNum0(){
-    if(document.getElementById("input").value == "0")
+    if(document.getElementById("input").value == 0)
     {
-        document.getElementById("input").value = String(0);
+        document.getElementById("input").value = 0;
     }
     else{
-        document.getElementById("input").value += String(0);
+        document.getElementById("input").value += 0;
     } 
 }
 function decimal(){
@@ -101,25 +102,26 @@ function decimal(){
 }
 function krat(){
     predesla_hodnota += Number(document.getElementById("input").value);
-    document.getElementById("input").value = "";
+    document.getElementById("input").value = 0;
     decimal_ready = true;
     operace = "x"
 }
 function plus(){
-    predesla_hodnota += Number(document.getElementById("input").value);
-    document.getElementById("input").value = "";
+    predesla_hodnota = predesla_hodnota + Number(document.getElementById('input').value)
+    console.log(predesla_hodnota)
+    document.getElementById("input").value = 0;
     decimal_ready = true;
     operace = "+"
 }
 function minus(){
     predesla_hodnota += Number(document.getElementById("input").value);
-    document.getElementById("input").value = "";
+    document.getElementById("input").value = 0;
     decimal_ready = true;
     operace = "-"
 }
 function deleno(){
     predesla_hodnota += Number(document.getElementById("input").value);
-    document.getElementById("input").value = "";
+    document.getElementById("input").value = 0;
     decimal_ready = true;
     operace = "/"
 }
@@ -131,30 +133,32 @@ function abs(){
 }
 function ac(){
     document.getElementById("input").value = 0;
+    predesla_hodnota = 0
+    decimal_ready = true;
 }
 function equal(){
-    hodnota_ted = document.getElementById("input").value;
+    hodnota_ted = Number(document.getElementById("input").value);
     console.log(predesla_hodnota, hodnota_ted);
-    if(predesla_hodnota == "")
+    if(predesla_hodnota == 0)
     {
         document.getElementById("input").value = hodnota_ted;
     }
     else if(operace == "+")
     {
-        document.getElementById("input").value = String(Number(predesla_hodnota) + Number(hodnota_ted))
+        document.getElementById("input").value = (Number(predesla_hodnota) + Number(hodnota_ted))
     }
     else if(operace == "x")
     {
-        document.getElementById("input").value = String(Number(predesla_hodnota) * Number(hodnota_ted))
+        document.getElementById("input").value = (Number(predesla_hodnota) * Number(hodnota_ted))
     }
     else if(operace == "/")
     {
-        document.getElementById("input").value = String(Number(predesla_hodnota) / Number(hodnota_ted))
+        document.getElementById("input").value = (Number(predesla_hodnota) / Number(hodnota_ted))
     }
     else if(operace == "-")
     {
-        document.getElementById("input").value = String(Number(predesla_hodnota) - Number(hodnota_ted))
+        document.getElementById("input").value = (Number(predesla_hodnota) - Number(hodnota_ted))
     }
     decimal_ready = true;
-    predesla_hodnota = "";
+    predesla_hodnota = 0;
 }
